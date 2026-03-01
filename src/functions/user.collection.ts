e147a21bbe81221f9ee0a238cabdfd99bf28c157
Bastiaan <bas@strootje.com>
2026-03-01 14:14:26 +0100

    fix: list users as clinet only

--- src/functions/user.collection.ts
+++ src/functions/user.collection.ts
@@ -2,6 +2,11 @@ import { clientAuth } from "@scope/auth/client";
 import { singleQueryClient } from "@strootje/more/query";
 import { queryCollectionOptions } from "@tanstack/query-db-collection";
 import { createCollection } from "@tanstack/solid-db";
+import { createClientOnlyFn } from "@tanstack/solid-start";
+
+const listUsers = createClientOnlyFn(() => {
+  return clientAuth.admin.listUsers({ query: {} });
+});
 
 export const UserCollection = createCollection(queryCollectionOptions({
   queryClient: singleQueryClient(),
@@ -9,7 +14,7 @@ export const UserCollection = createCollection(queryCollectionOptions({
   getKey: (item) => item.id,
 
   queryFn: async () => {
-    const { data } = await clientAuth.admin.listUsers({ query: {} });
+    const { data } = await listUsers();
     return data?.users ?? [];
   },
 }));
