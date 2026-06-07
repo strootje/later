c4611c4572d2e90bac5784e9233b06286f648b9e
Bastiaan <bas@strootje.com>
2026-06-07 12:17:34 +0200

    fix: added temporal polyfill

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,6 +1,7 @@
 import * as clientDb from "@scope/db/client";
 import { hydrateStart, StartClient } from "@tanstack/solid-start/client";
 import { hydrate } from "solid-js/web";
+import "temporal-polyfill/global";
 import { useRegisterSW } from "virtual:pwa-register/solid";
 
 useRegisterSW({
