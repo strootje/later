f4321095de7fa6d2f8ae8a66e5ca51f8f9124fe3
Bastiaan <bas@strootje.com>
2026-06-08 13:47:30 +0200

    fix: background colors

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -3,11 +3,25 @@ import { combinePresetAndAppleSplashScreens, defineConfig, minimal2023Preset } f
 export default defineConfig({
   images: ["public/favicon.svg"],
   preset: combinePresetAndAppleSplashScreens(
-    minimal2023Preset,
     {
-      resizeOptions: {
-        background: "#265456",
+      ...minimal2023Preset,
+      maskable: {
+        ...minimal2023Preset.maskable,
+        resizeOptions: {
+          ...minimal2023Preset.maskable.resizeOptions,
+          background: "#265456",
+        },
       },
+      apple: {
+        ...minimal2023Preset.apple,
+        resizeOptions: {
+          ...minimal2023Preset.apple.resizeOptions,
+          background: "#265456",
+        },
+      },
+    },
+    {
+      resizeOptions: {},
     },
   ),
 });
